19aeb78a0a45382ea94db7766a3e8729db512560
hryoshihara0692 <hr.yoshihara0692@gmail.com>
2023-07-10 12:43:01 +0900

    Initial Commit

---
+++ Reminder/ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderApp.swift
+//  Reminder
+//
+//  Created by 吉原博基 on 2023/07/10.
+//
+
+import SwiftUI
+
+@main
+struct ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
